bd93edec043de1a78c20f9022073589cc98a3b3a
1201dc <90306670@ep-student.org>
2021-11-01 00:08:09 -0700

    Initial Commit

---
+++ Barkly/BarklyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarklyApp.swift
+//  Barkly
+//
+//  Created by Dhruv Chowdhary on 11/1/21.
+//
+
+import SwiftUI
+
+@main
+struct BarklyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
